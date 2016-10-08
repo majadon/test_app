8c68d1160a27f4d881224e981cb059ae64971455
Prelang Builder <builder@prelang.com>
2016-10-08 10:14:16 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -3,7 +3,7 @@ class ArticleCommentsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_article_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_article_comment, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /article_comments
   # GET /article_comments.json
